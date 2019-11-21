4fbac42b7ed6be5743438fb9435ad79c3b5e766b
Karin Norrlander <karin@barprego.se>
2019-11-21 11:57:11 +0100

    further development on notemanager

--- src/BoomLibrary/NoteManager.cs
+++ src/BoomLibrary/NoteManager.cs
@@ -1,33 +1,81 @@
 namespace BoomLibrary
 {
 
-    public enum NoteView{Basic, Light, }
+    //Enum styr vilken view som ska visas av metod för att visa Note
+    public enum NoteView{ Basic=1, Light, Expanded }
+    
     public class NoteManager
     {
         //variabel som håller håller koll på när senaste review gjordes
-        public int 
+        public int ReviewTime =0;
 
         //metod som tar bort en note
         public void DeleteNote()
         {
 
         }
-
+        
         //metod som visar en note 
         //kallar på andra metoder beroende på inparametrar
         //En enum styr vilken vy som kommer att visas 
-        //Visa basic ToString()
+
+        public string ShowNote(NoteView view)
+        {
+            switch(view)
+            {
+                case 1:
+                return;
+
+                case 2:
+                return;
+
+                case 3:
+                return;
+
+                default:
+            }
+        }
+        
+        //Visar Basic ToString 
+        private string GetBasicView()
+        {
+
+        }
+        
         //Light view, titel plus status
+        private string GetLightView(Note note)
+        {
+            string title = note.Title;
+            Status status = note.Status;
+            return "Denna Note har titel: " + titel + " med status: " + status;
+        }
+
         //Expanded view tom 50 tecken i beskrivningen
 
+        private string GetExpandedView()
+        {
+
+        }
 
         //Metod som ändrar status på en note
-
         public int ChangeStatus(int status)
         {
             return status;
         }
-        //
+        
+        //Metod som flyttar Note framåt i flödet
+        public int MoveNoteForward()
+        {
+
+        }
+
+        //Metod som returnerar Booms
+
+        public string RewardWithBooms(int boomcounter)
+        {
+            string str = "";
+        }
     }
+}
 
 
