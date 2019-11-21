2c4acebac2fa6e311a92cc8e19445d71d537b91a
Thomas Karlsson <thomas.karlsson.tk@gmail.com>
2019-11-21 10:49:14 +0100

    Co-authored-by: Mia Kling <vovve73@icloud.com> Co-authored-by: KarinNorr <karin@barprego.se> Co-authored-by: Lucas Wiseby <l.wiseby@gmail.com>

---
+++ src/BoomLibrary/NoteManager.cs
@@ -0,0 +1,33 @@
+namespace BoomLibrary
+{
+
+    public enum NoteView{Basic, Light, }
+    public class NoteManager
+    {
+        //variabel som håller håller koll på när senaste review gjordes
+        public int 
+
+        //metod som tar bort en note
+        public void DeleteNote()
+        {
+
+        }
+
+        //metod som visar en note 
+        //kallar på andra metoder beroende på inparametrar
+        //En enum styr vilken vy som kommer att visas 
+        //Visa basic ToString()
+        //Light view, titel plus status
+        //Expanded view tom 50 tecken i beskrivningen
+
+
+        //Metod som ändrar status på en note
+
+        public int ChangeStatus(int status)
+        {
+            return status;
+        }
+        //
+    }
+
+
---
+++ src/BoomLibrary/User.cs
@@ -0,0 +1,16 @@
+namespace BoomLibrary
+{
+    public class User
+    {
+        private string name;
+        private int boomSum;
+        // TODO: Id Guid  
+        // TODO: List<Notes> notes = new List<Notes>();       
+        // Konstruktor
+        p
+        static void CreatUser()
+        {
+            var user = new User();
+        }
+    }
+}
